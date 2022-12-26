181e48a8159436f927818741446027908478c4ed
mhd-atheel <aathilmazz1234@gmail.com>
2022-12-26 23:58:48 +0530

    check Valid

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -218,3 +218,26 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
+
+String? validateEmail(String? formEmail) {
+  if (formEmail == null || formEmail.isEmpty)
+    return 'E-mail address is required.';
+  String pattern = r'\w+@\w+\.\w+';
+  RegExp regex = RegExp(pattern);
+  if (!regex.hasMatch(formEmail)) return 'Invalid E-mail Address format.';
+  return null;
+}
+
+String? validatePassword(String? formPassword) {
+  if (formPassword == null || formPassword.isEmpty)
+    return 'Password is required.';
+  String pattern =
+      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+  RegExp regex = RegExp(pattern);
+  if (!regex.hasMatch(formPassword))
+    return '''
+      Password must be at least 8 characters,
+      include an uppercase letter, number and symbol.
+      ''';
+  return null;
+}
