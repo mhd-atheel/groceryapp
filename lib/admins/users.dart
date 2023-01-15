a3408aa5d7aba26307af1aa15c92ba602fb3c57f
mhd-atheel <aathilmazz1234@gmail.com>
2023-01-15 22:43:53 +0530

    adding admin features and display admin

--- lib/admins/users.dart
+++ lib/admins/users.dart
@@ -1,8 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:groceryapp/admins/adminHome.dart';
+
+
+
 class Users extends StatefulWidget {
   const Users({Key? key}) : super(key: key);
 
@@ -11,16 +15,15 @@ class Users extends StatefulWidget {
 }
 
 class _UsersState extends State<Users> {
+  bool isAdmin =false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text(
+        title: const Text(
           "Users",
           style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 25,
-              fontFamily: "Poppins"),
+              fontWeight: FontWeight.bold, fontSize: 25, fontFamily: "Poppins"),
         ),
         backgroundColor: Color(0xffF4F4F3),
         foregroundColor: Color(0xff2C5E30),
@@ -33,7 +36,11 @@ class _UsersState extends State<Users> {
               MaterialPageRoute(builder: (context) => const AdminHome()),
             );
           },
-          icon: Icon(FontAwesomeIcons.chevronLeft,size: 20,color: Color(0xff2C5E30),),
+          icon: Icon(
+            FontAwesomeIcons.chevronLeft,
+            size: 20,
+            color: Color(0xff2C5E30),
+          ),
         ),
       ),
       body: StreamBuilder(
@@ -49,11 +56,14 @@ class _UsersState extends State<Users> {
           return ListView(
             shrinkWrap: true,
             children: snapshot.data!.docs.map((DocumentSnapshot document) {
-              Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+              Map<String, dynamic> data =
+                  document.data()! as Map<String, dynamic>;
               return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 4),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4),
                 child: ListTile(
-                  shape: RoundedRectangleBorder( //<-- SEE HERE
+                  shape: RoundedRectangleBorder(
+                    //<-- SEE HERE
                     side: BorderSide(width: 1),
                     borderRadius: BorderRadius.circular(20),
                   ),
@@ -64,55 +74,145 @@ class _UsersState extends State<Users> {
                       imageBuilder: (context, imageProvider) => CircleAvatar(
                         backgroundImage: imageProvider,
                       ),
-                      placeholder: (context, url) => CircularProgressIndicator(),
+                      placeholder: (context, url) =>
+                          CircularProgressIndicator(),
                       errorWidget: (context, url, error) => Icon(Icons.error),
                     ),
                   ),
-                  title: Text(
-                    data['name'],
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        data['name'],
+                        style: TextStyle(
+                            fontFamily: 'Poppins', fontWeight: FontWeight.w400),
+                      ),
+
+                      data['userType']=='admin'?Container(
+                        height: 18,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.amber,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 4.0,vertical: 2),
+                          child: Center(
+                            child: Text(
+                              'Admin',
+                              style: TextStyle(
+                                  fontFamily: 'Poppins', fontWeight: FontWeight.bold,fontSize: 10,color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ):Container(),
+                    ],
+                  ),
+                  subtitle: Text(
+                    data['email'],
                     style: TextStyle(
                       fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w400
+                      fontWeight: FontWeight.w200,
+                      fontSize: 12,
                     ),
                   ),
-                  subtitle: Text(data['email'],
-                      style: TextStyle(
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w200,
-                        fontSize: 12,
-                  ),
-                  ),
                   trailing: GestureDetector(
-                    onTap:  (){
-                          showDialog<void>(
+                      onTap: () {
+                        showDialog<void>(
                           context: context,
                           builder: (BuildContext context) {
-                          return AlertDialog(
-                          title: const Text('Are you sure to Delete?',style: TextStyle(fontFamily: 'Prompt'),),
-                          actions: <Widget>[
-                          TextButton(
-                          style: TextButton.styleFrom(
-                          textStyle: Theme.of(context).textTheme.labelLarge,
-                          ),
-                          child: const Text('Delete',style: TextStyle(color: Colors.red,fontFamily: 'Prompt'),),
-                          onPressed: () {
-                          Navigator.of(context).pop();
+                            return AlertDialog(
+                              title: Center(
+                                child: const Text(
+                                  'Are you sure to ?',
+                                  style: TextStyle(fontFamily: 'Prompt'),
+                                ),
+                              ),
+                              actions: <Widget>[
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Container(
+                                    height: 40,
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        border: Border.all(
+                                            color: Colors.green,
+                                            width: 2
+                                        ),
+                                        borderRadius: BorderRadius.circular(10)
+                                    ),
+                                    child: TextButton(
+                                      style: TextButton.styleFrom(
+                                        textStyle: Theme.of(context).textTheme.labelLarge,
+                                      ),
+                                      child: Center(child: data['userType']=='user'?Text('Make Admin',style: TextStyle(fontFamily: 'Prompt',color: Colors.green,fontWeight: FontWeight.bold),):
+                                      Text('Remove Admin',style: TextStyle(fontFamily: 'Prompt',color: Colors.green,fontWeight: FontWeight.bold),)),
+                                      onPressed: () async {
+                                        FirebaseFirestore.instance.collection('biodata').doc(document.id).get().
+                                        then((value){
+                                          String userType = value.data()!['userType'];
+                                          if(userType=='user'){
+                                            FirebaseFirestore.instance.collection('biodata').doc(document.id).update(
+                                              {
+                                                'userType':'admin'
+                                              }
+                                            ).then((value) {
+                                              setState(() {
+                                                isAdmin=true;
+                                              });
+                                              Navigator.pop(context);
+                                            });
+                                          }
+                                          if(userType=='admin'){
+                                            FirebaseFirestore.instance.collection('biodata').doc(document.id).update(
+                                                {
+                                                  'userType':'user'
+                                                }
+                                            ).then((value){
+                                              Navigator.pop(context);
+                                            });
+                                          }
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Container(
+                                    height: 40,
+                                    decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        border: Border.all(
+                                            color: Colors.red,
+                                            width: 2
+                                        ),
+                                        borderRadius: BorderRadius.circular(10)
+                                    ),
+                                    child: TextButton(
+                                      style: TextButton.styleFrom(
+                                        textStyle: Theme.of(context).textTheme.labelLarge,
+                                      ),
+                                      child: Center(child: const Text('Delete',style: TextStyle(fontFamily: 'Prompt',color: Colors.red,fontWeight: FontWeight.bold),)),
+                                      onPressed: () async {
+                                      },
+                                    ),
+                                  ),
+                                ),
+                                TextButton(
+                                  style: TextButton.styleFrom(
+                                    textStyle: Theme.of(context).textTheme.labelLarge,
+                                  ),
+                                  child: Center(child: const Text('Cancel',style: TextStyle(fontFamily: 'Prompt'),)),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+
+                              ],
+                            );
                           },
-                          ),
-                          TextButton(
-                          style: TextButton.styleFrom(
-                          textStyle: Theme.of(context).textTheme.labelLarge,
-                          ),
-                          child: const Text('Cancel',style: TextStyle(fontFamily: 'Prompt'),),
-                          onPressed: () {
-                          Navigator.of(context).pop();
-                          },
-                          ),
-                          ],
-                          );
-                          },
-                          );
-                          },
+                        );
+                      },
                       child: Icon(Icons.more_vert)),
                 ),
               );
